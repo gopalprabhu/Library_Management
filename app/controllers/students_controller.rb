4f4d230d488a71a31bf4af1341add2134da472cd
Gopalakrishna Prabhu <gopalprabhu888@gmail.com>
2020-02-15 02:28:09 +0530

    Changed label student to ID

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -69,6 +69,6 @@ class StudentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def student_params
-      params.require(:student).permit(:name, :student_id, :contact)
+      params.require(:student).permit(:name, :c, :contact)
     end
 end
--- app/views/students/_student.json.jbuilder
+++ app/views/students/_student.json.jbuilder
@@ -1,2 +1,3 @@
 json.extract! student, :id, :name, :student_id, :contact, :created_at, :updated_at
 json.url student_url(student, format: :json)
+
